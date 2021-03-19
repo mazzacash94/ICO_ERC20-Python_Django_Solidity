ef12c135f5e0fcd6fc697c90cecd04fac86e313e
mazzacash94 <gianmariamazza94@gmail.com>
2021-03-18 18:48:09 -0700

    Remove directories

--- venv/Lib/site-packages/pipx/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if sys.version_info < (3, 6, 0):
-    sys.exit(
-        "Python 3.6 or later is required. "
-        "See https://github.com/pipxproject/pipx "
-        "for installation instructions."
-    )
